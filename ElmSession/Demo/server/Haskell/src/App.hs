58e2bae32efe7980627dea64a785444f03771f27
Carsten Koenig <Carsten@gettingsharper.de>
2018-06-14 11:30:47 +0200

    Haskell: Api und Sqlite

--- ElmSession/Demo/server/Haskell/src/App.hs
+++ ElmSession/Demo/server/Haskell/src/App.hs
@@ -1,24 +1,37 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts  #-}
+
+
 module App
     ( startApp
     , app
     ) where
 
+
+import           Control.Monad.IO.Class (liftIO)
 import           Data.Aeson ()
 import           Data.Aeson.TH ()
-import           Models (User, users)
+import           Data.ByteString.Char8 (pack)
+import           Data.Text (Text)
+import qualified Db
 import           Network.Wai (Application)
 import           Network.Wai.Handler.Warp (run)
 import qualified Page
-import           Servant (Server, JSON, Proxy(..), Raw, Get, (:<|>)(..), (:>))
-import qualified Servant
+import           Servant
 import           Servant.HTML.Blaze (HTML)
 import           Text.Blaze.Html4.Strict (Markup)
 
 
 type API = 
-  "users" :> Get '[JSON] [User]
+  ( "todos" :> (
+    Get '[JSON] [Db.Task]
+    :<|> ReqBody '[JSON] Db.Task :> Post '[JSON] Db.Task
+    :<|> "new" :> ReqBody '[JSON] Text :> Post '[JSON] Db.Task
+    :<|> Capture "id" Db.TaskId :> Get '[JSON] (Maybe Db.Task)
+    )
+  )
   :<|> Get '[HTML] Markup
   :<|> "static" :> Raw
 
@@ -27,32 +40,65 @@ getPort :: IO Int
 getPort = return 8080
 
 
+getDbPath :: IO FilePath
+getDbPath = return "./todos.db"
+
+
 startApp :: IO ()
 startApp = do
+  dbPath <- getDbPath
+  putStrLn $ "initializing Database in " ++ dbPath
+  Db.initDb dbPath
+
   port <- getPort
   putStrLn $ "starting Server on " ++ show port
-  run port app
+  run port $ app dbPath
 
 
-app :: Application
-app = Servant.serve api server
+app :: FilePath -> Application
+app = Servant.serve api . server 
 
 
 api :: Proxy API
 api = Proxy
 
 
-server :: Server API
-server = 
-  usersHandler 
+server :: FilePath -> Server API
+server dbFile = 
+  todoHandlers 
   :<|> pageHandler
   :<|> staticHandler
   where
-    usersHandler =
-      return users
+    todoHandlers =
+      getAllHandler :<|> updateHandler :<|> newHandler :<|> queryHandler
+
+    getAllHandler =
+      Db.withConnection dbFile Db.listTasks
+
+    updateHandler task = do
+      liftIO $ putStrLn $ "updating task " ++ show (Db.id task)
+      Db.withConnection dbFile $ Db.modifyTask task
+      throwError $ redirect (Db.id task)
+
+    newHandler txt = do
+      tId <- Db.withConnection dbFile $ Db.insertTask txt
+      liftIO $ putStrLn $ "created new task - redirecting to " ++ show tId
+      throwError $ redirect tId
+
+    queryHandler tId = do
+      liftIO $ putStrLn $ "getting task " ++ show tId
+      Db.withConnection dbFile $ Db.getTask tId
 
     pageHandler = 
       return Page.index
 
     staticHandler =
       Servant.serveDirectoryWebApp "../../static"
+
+
+redirect :: Db.TaskId -> ServantErr
+redirect tId = addLoc tId err303
+  where
+    addLoc tId err =
+      let headers = ("Location", pack ("/todos/" ++ show tId)) : errHeaders err
+      in err { errHeaders = headers }
---
+++ ElmSession/Demo/server/Haskell/src/Db.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Db
+  ( TaskId
+  , Task (..)
+  , initDb
+  , withConnection
+  , getTask
+  , listTasks
+  , insertTask
+  , deleteTask
+  , modifyTask
+  ) where
+
+import           Control.Monad.IO.Class (MonadIO, liftIO)
+import           Data.Aeson.TH (deriveJSON, defaultOptions)
+import           Data.Int (Int64)
+import           Data.Maybe (listToMaybe)
+import           Data.Text (Text)
+import           Database.SQLite.Simple (NamedParam(..))
+import qualified Database.SQLite.Simple as Sql
+
+
+type TaskId = Int64
+
+data Task = Task { id       :: TaskId
+                 , text     :: Text
+                 , finished :: Bool
+                 } deriving Show
+
+$(deriveJSON defaultOptions ''Task)
+
+
+initDb :: MonadIO m => FilePath -> m ()
+initDb file = withConnection file $ \conn ->
+  Sql.execute_ conn
+    "CREATE TABLE IF NOT EXISTS todos (\
+    \task NVARCHAR(255) NOT NULL, \
+    \finished BOOLEAN NOT NULL )"
+
+
+withConnection :: MonadIO m => FilePath -> (Sql.Connection -> IO a) -> m a
+withConnection file m = 
+  liftIO $ Sql.withConnection file m
+
+
+getTask :: TaskId -> Sql.Connection -> IO (Maybe Task)
+getTask tId conn =
+  listToMaybe . map toTask <$> Sql.query conn "SELECT task,finished FROM todos WHERE rowid = ?" (Sql.Only tId)
+  where
+    toTask (txt,fin) = Task tId txt fin
+
+
+listTasks :: Sql.Connection -> IO [Task]
+listTasks conn = 
+  map toTask <$> Sql.query_ conn "SELECT rowid,task,finished FROM todos"
+  where
+    toTask (tId,txt,fin) = Task tId txt fin
+
+
+insertTask :: Text -> Sql.Connection -> IO TaskId
+insertTask txt conn = do
+  Sql.execute conn "INSERT INTO todos (task,finished) VALUES (?,0)" (Sql.Only txt)
+  Sql.lastInsertRowId conn
+
+
+deleteTask :: TaskId -> Sql.Connection -> IO ()
+deleteTask tId conn =
+  Sql.execute conn "DELETE FROM todos WHERE rowid=?" (Sql.Only tId)
+
+
+modifyTask :: Task -> Sql.Connection -> IO ()
+modifyTask (Task tId txt fin) conn =
+  Sql.executeNamed conn "UPDATE todos SET task = :task, finished = :finished WHERE rowid = :id" [":id" := tId, ":task" := txt, ":finished" := fin]
