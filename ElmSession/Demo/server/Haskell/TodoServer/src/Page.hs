8e796ea133017383255a28d2021cba1169eac585
Carsten Koenig <Carsten@gettingsharper.de>
2018-06-14 09:01:13 +0200

    Elm-Output benutzen

--- ElmSession/Demo/server/Haskell/TodoServer/src/Page.hs
+++ ElmSession/Demo/server/Haskell/TodoServer/src/Page.hs
@@ -4,6 +4,7 @@ module Page
   ( index
   ) where
 
+import           Data.Foldable (traverse_)
 import           Text.Blaze.Html4.Strict (Markup, (!))
 import qualified Text.Blaze.Html4.Strict as H
 import qualified Text.Blaze.Html4.Strict.Attributes as A
@@ -12,7 +13,29 @@ import qualified Text.Blaze.Html4.Strict.Attributes as A
 index :: Markup
 index = H.html $ do
   H.head $ do
-    H.title "Todo List in Elm"
-    H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "static/style.css"
-  H.body $
-    H.h1 "Hello Blaze!"
+    H.title "Todo in Elm"
+    links
+  H.body scripts
+
+
+links :: H.Html
+links =
+  traverse_ (\ref -> H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href ref) cssSrcs
+  where
+    cssSrcs =
+      [ "static/lib/bootstrap/dist/css/bootstrap.min.css"
+      , "static/style.css"
+      ]
+
+scripts :: H.Html
+scripts = do
+  traverse_ (\ref -> H.script "" ! A.src ref) jsSrcs
+  H.script $ H.toHtml $ unlines
+    [ "var app = Elm.Main.fullscreen({baseUrl: '/' });" ]
+
+  where
+    jsSrcs =
+      [ "static/lib/jquery/jquery.js"
+      , "static/lib/popper/dist/js/popper.min.js"
+      , "static/todo.js"
+      ]
