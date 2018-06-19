0140367438233c7d522761ca70e4890ff1358198
Carsten Koenig <Carsten@gettingsharper.de>
2018-06-19 13:07:43 +0200

    Haskell Backend leitet alle URL-Segemente zur Index Seite weiter

--- ElmSession/Demo/server/Haskell/src/RouteApi.hs
+++ ElmSession/Demo/server/Haskell/src/RouteApi.hs
@@ -10,17 +10,9 @@ module RouteApi
     ) where
 
 
-import           Control.Monad.IO.Class (liftIO)
-import           Control.Monad.Trans (lift)
-import           Control.Monad.Reader (ReaderT, runReaderT)
-import           Control.Monad.Except (ExceptT, runExceptT)
 import           Data.Aeson ()
 import           Data.Aeson.TH ()
-import           Data.ByteString.Char8 (pack)
 import           Data.Text (Text)
-import qualified Db
-import           Network.Wai (Application)
-import           Network.Wai.Handler.Warp (run)
 import qualified Page
 import           Servant
 import           Servant.HTML.Blaze (HTML)
@@ -28,14 +20,15 @@ import           Text.Blaze.Html4.Strict (Markup)
 
 
 type RouteApi =
-  Get '[HTML] Markup
-  :<|> "static" :> Raw
+  "static" :> Raw
+  :<|> CaptureAll "segments" Text :> Get '[HTML] Markup
+
 
 
 server :: Server RouteApi
-server = pageHandler :<|> staticHandler
+server = staticHandler :<|> pageHandler
   where
-    pageHandler =
+    pageHandler _ =
       return Page.index
 
     staticHandler =
