baff058637fae62166e75fe27bee25e88ddf52cb
Carsten König <carsten@gettingsharper.de>
2018-06-25 14:30:43 +0200

    TEA Demo ~> einfache TODO Liste

--- ElmSession/Demo/client/TEA/src/Main.elm
+++ ElmSession/Demo/client/TEA/src/Main.elm
@@ -2,7 +2,41 @@ module Main exposing (main)
 
 import Html exposing (..)
 import Html.Events as Ev
+import Html.Attributes as Attr
+
+
+type alias Model =
+    { input : String
+    , tasks : List String
+    }
+
+
+view model =
+    div []
+        [ form []
+            [ input
+                [ Attr.value model.input
+                ]
+                []
+            , button [] [ text "OK" ]
+            ]
+        , viewTasks model.tasks
+        ]
+
+
+viewTasks tasks =
+    ul []
+        (List.map (\task -> li [] [ text task ]) tasks)
+
+
+update msg model =
+    model
 
 
 main =
-    text "Hello Elm"
+    beginnerProgram
+        { model =
+            Model "" [ "TEA vorstellen" ]
+        , view = view
+        , update = update
+        }
